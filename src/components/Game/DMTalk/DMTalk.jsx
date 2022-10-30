21d4d0738271e8cea3fb43f2189e2d598ce21079
Joe Malatesta <joemalatesta@msn.com>
2022-10-29 20:28:23 -0400

    adjust the beginning text, make random roll.

--- src/components/Game/DMTalk/DMTalk.jsx
+++ src/components/Game/DMTalk/DMTalk.jsx
@@ -6,30 +6,27 @@ import D20 from '../../D20/D20'
 
 const DMTalk = (props) => {
   console.log(props.dMNumCode);
-  const [dieView, setDieView] = useState()
-
-  const toggleDieView = () => setDieView(!dieView)
 
 
+  const getRandomNumber = () =>{
+    let num = Math.floor(Math.random() * 100)
+    console.log(num);
+    return num
+  }
 
   const spoken = () => {
     if(isNaN(props.dMNumCode)) {
-      return <p hidden={props.dMView? true : false}  > {texts[0].text}  </p>
+      return  texts[0].text
     }
-    if(props.dMNumCode === 3254){
+    if(props.dMNumCode === 3254 || getRandomNumber() > 90){
       return(
       <div  hidden={false}>
-     
-        <p> {texts[1].text} </p>
+        {texts[1].text}        
         <D20 />
       </div>)
     }  
   }
-
-
-
-
-
+  
   return ( 
       <>
         {spoken()}
